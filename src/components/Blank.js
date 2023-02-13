b1189fa5ba8602ed14a878113816b4fcddadc540
Dyanesh Dhawale <dyanixdhawale@gmail.com>
2023-02-13 18:37:21 +0530

    img

--- src/components/Blank.js
+++ src/components/Blank.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Blank = () => {
   return (
-    <div align="center"><h1 align="center">Hi <img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" height="30px" />, I'm Dyanesh Dhawale</h1>
+    <div align="center"><h1 align="center">Hi <img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" height="30px" /></h1>
 
-<img  src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif" height="250px"  align="center" />
+<img  src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif" height="250px"  align="center" style={{ borderRadius: "50%" }}/>
     <h1 align="center">This application is only optimized for desktop view. Please view on a larger screen for optimal experience.</h1>
     </div>
   )
