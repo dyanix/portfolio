60b9b9a5a5cce5b125ddab5ba309dcf12422f578
Dyanesh Dhawale <dyanixdhawale@gmail.com>
2023-03-29 01:07:48 +0530

    c

--- src/components/Blank.js
+++ src/components/Blank.js
@@ -4,9 +4,9 @@ const Blank = () => {
   return ( // eslint-disable-next-line 
     <div align="center"><h1 align="center">Hi <img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" height="30px" /></h1>
 
-      <img src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif" height="250px" align="center" style={{ borderRadius: "50%" }} />
+      <img src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif" height="250px" align="center" style={{ borderRadius: "50%"  ,marginLeft:"-90px" }} />
       
-      <h1 align="center">This application is only optimized for desktop view. Please view on a larger screen for optimal experience.</h1>
+      <p align="center">This application is only optimized for desktop view. Please view on a larger screen for optimal experience.</p>
     </div>
   )
 }
