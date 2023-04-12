ad564f61a54f583b68af234357459ebcf783ddb8
Dyanesh Dhawale <dyanixdhawale@gmail.com>
2023-04-13 02:19:05 +0530

    blank screen  and resume

--- src/components/Blank.js
+++ src/components/Blank.js
@@ -1,17 +1,64 @@
-import React from 'react'
+import React from 'react';
 
 const Blank = () => {
-  return ( // eslint-disable-next-line 
-    <div align="center"><h1 align="center">Hi <img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" height="30px" /></h1>
+  return (
+    <div className="container">
+      <h1>Hi <img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" alt="Wave hand gesture"  height="30px" /></h1>
 
-      <img src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif" height="250px" align="center" style={{ borderRadius: "50%"  ,marginLeft:"-90px" }} />
-      
-      <p align="center">This application is only optimized for desktop view. Please view on a larger screen for optimal experience.</p>
+      <img src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif" alt="Wave hand gesture" height="250px" style={{ borderRadius: "50%" }} />
+
+      <p>This application is only optimized for desktop view. Please view on a larger screen for optimal experience.</p>
+
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          text-align: center;
+          padding: 1rem;
+        }
+
+        h1 {
+          font-size: 2rem;
+          margin-top: 2rem;
+        }
+
+        img {
+          margin-top: 2rem;
+          margin-bottom: 2rem;
+        }
+
+        p {
+          margin-bottom: 2rem;
+        }
+
+        @media (min-width: 768px) {
+          .container {
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: flex-start;
+            text-align: left;
+            padding: 2rem;
+          }
+
+          h1 {
+            font-size: 3rem;
+            margin-top: 0;
+          }
+
+          img {
+            margin-top: 0;
+            margin-bottom: 0;
+            margin-right: 2rem;
+          }
+
+          p {
+            margin-bottom: 0;
+          }
+        }
+      `}</style>
     </div>
-  )
+  );
 }
 
-export default Blank
-
-
-
+export default Blank;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import "./Navbar.css"
 
-import { AiFillLinkedin, AiOutlineHome ,AiOutlineUser ,AiOutlineGithub} from 'react-icons/ai';
+import { AiFillLinkedin, AiOutlineFileText, AiOutlineUser, AiOutlineGithub } from 'react-icons/ai';
 
 
 const Navbar = () => {
- 
+
   return (
 
     <div className="navbar-container" >
       <ul>
-        
+
         <li style={{ "--i": "#56ccf2", "--j": "#2f80ed" }}>
           <span class="icon">
             < AiOutlineUser style={{ color: '#56ccf2' }} />
           </span>
           <a class="titles" href="mailto:dyanixdhawale@gmail.com?subject=Enquiry"><span class="title">Message</span></a>
-         
+
         </li>
         <li style={{ "--i": "#ff9966", "--j": "#ff5e62" }}>
           <span class="icon">
-              <AiOutlineGithub style={{ color: 'black' }} />
-            </span>
+            <AiOutlineGithub style={{ color: 'black' }} />
+          </span>
           <a class="titles" href="https://github.com/dyanix" target="_blank" rel="noopener noreferrer"><span class="title">Github</span>
           </a>
 
@@ -36,8 +36,18 @@ const Navbar = () => {
           </span>
           <a class="titles" href="https://www.linkedin.com/in/dyanesh-dhawale-8742241b9/" target="_blank" rel="noopener noreferrer"> <span class="title">LinkDin</span>
           </a>
-         
+
         </li>
+
+        <li style={{ "--i": "#ff6", "--j": "purple" }}>
+          <span class="icon">
+            <AiOutlineFileText style={{ color: 'black' }} />
+          </span>
+          <a class="titles" href="https://drive.google.com/file/d/1LQEN_DqHGzRZik7T5RUlIYz0diiF6g4u/view?usp=share_link" download target="_blank" rel="noopener noreferrer">
+            <span class="title">Resume</span>
+          </a>
+        </li>
+
       </ul>
 
     </div>
